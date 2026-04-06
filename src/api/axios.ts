edf46d4b44f8d077af07cf3bffd1889bfeaa59fb
exolonConfidental <shikharsingh3324@gmail.com>
2026-04-06 18:50:30 +0530

    5-added backed url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create(
     {
-        baseURL: "http://localhost:3000/api",
+        baseURL: "https://finance-tracker-backend-z71j.onrender.com/api",
         headers: {
             "Content-Type": "application/json",
         },
